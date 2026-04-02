10bf0816c0fd1f511b42c13fd275578ade83cfa6
nob <no.nob.n5.biz@gmail.com>
2026-04-03 05:24:48 +0900

    最終確認

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    public function index()
+    {
+        return view('register');
+    }
+
     public function register(RegisterRequest $request)
     {
         $user = User::create([
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ConfirmController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +23,5 @@ Route::get('/contact/confirm', function (){return redirect('/');});
 Route::post('/contact', [ContactController::class, 'store']);
 Route::get('/admin', [AdminController::class, 'index']);
 Route::delete('/admin/{id}', [AdminController::class, 'destroy']);
-
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'register']);
